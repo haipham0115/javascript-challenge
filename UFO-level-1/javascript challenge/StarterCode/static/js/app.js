7e8c28f60fb81c0da2b4ba0c3ea80afd30e41445
Kai Pham <haipham0115@gmail.com>
2022-01-06 11:48:50 -0800

    finished level 1

--- UFO-level-1/javascript challenge/StarterCode/static/js/app.js
+++ UFO-level-1/javascript challenge/StarterCode/static/js/app.js
@@ -35,6 +35,19 @@ var tableData = data;
 //     }
 // }
 
+// function to make the table head
+function constructTableHead(table, data) {
+    var thead = table.createTHead();
+    var row = thead.insertRow();
+    for (var key of data) {
+        var th = document.createElement("th");
+        var text = document.createTextNode(key);
+        th.appendChild(text);
+        row.appendChild(th);
+    }
+}
+
+// function to fill the data into the actual table
 function fillingTable(table, data) {
     for (var element of data) {
         var row = table.insertRow();
@@ -46,36 +59,48 @@ function fillingTable(table, data) {
     }
 }
 
+// locate the element that will contain the table
 var ufoTable = document.getElementById("ufo-table");
-// var tableBody = ufoTable.getElementsByTagName("tbody")
-fillingTable(ufoTable, data);
+// grab the keys of the JSON object, will be used to make the table head
+var tableHead = Object.keys(data[0]);
 
+// calling the 2 function to fully construct the table
+fillingTable(ufoTable, data);
+constructTableHead(ufoTable, tableHead);
 
 
 // Filtering the data, using the input date 
 var filterButton = d3.select('#filter-btn');
 var filterTable = d3.select('#datetime');
 
-filterButton.on("click", runEnter);
-filterTable.on("submit", runEnter);
+filterButton.on("click", runFilter);
+filterTable.on("submit", runFilter);
 
 
-function runEnter() {
+function runFilter() {
     d3.event.preventDefault();
 
+    // Grabbing the input element
     var inputElement = d3.select('#datetime');
-
+    // retrieve the actual value of the input
     var inputValue = inputElement.property('value');
 
-    //testing,  IT WORKS BABY!!!!!!!
-    function whatever(datum) {
-        return datum.datetime === inputValue
-    };
-
+    // set the condition to filter, and create a new array object
     var filteredData = data.filter(datum => datum.datetime === inputValue);
     console.log(filteredData)
 
-    // passing the filterData as new parameter to fillingTable function
-    // call the function to reload the table with filtered input
+    // In order to load the new filtered table, I need to delete the current table from the original data
+    // grab the element that contain the table
+    var tableToDelete = d3.select("#ufo-table");
+    // remove all the children of inside the table, leaving the <table></table> blank
+    tableToDelete.html("");
+
+    // CONSTRUCT A NEW TABLE WITH `filteredData`
+
+
+    // Passing the filterData as new parameter to fillingTable function
+    // Call the function to reload the table with filtered input
     fillingTable(ufoTable, filteredData);
-}
+    // also, reconstruct the table head: same parameter as original is fine
+    constructTableHead(ufoTable, tableHead);
+};
